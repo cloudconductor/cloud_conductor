a3ed2e1aa8cd72d9179550d4950e9c7e518206c6
Yo Takezawa <takezawa.yo@tis.co.jp>
2014-05-20 10:33:06 +0900

    Add filter to restrict executable test by focus option.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,6 +25,10 @@ Spork.prefork do
   FactoryGirl.find_definitions
 
   RSpec.configure do |config|
+    # Enable focus feature to execute focused test only.
+    config.filter_run focus: true
+    config.run_all_when_everything_filtered = true
+
     config.before :suite do
       DatabaseCleaner.strategy = :truncation
       DatabaseCleaner.clean
