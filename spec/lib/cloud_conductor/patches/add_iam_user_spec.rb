442934ba2bfc9a1cb67b580872320e612435448e
konda <konda.tomoyuki@tis.co.jp>
2014-07-11 15:10:11 +0900

    Add patch to add AWS::IAM::User resource

---
+++ spec/lib/cloud_conductor/patches/add_iam_user_spec.rb
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+# Copyright 2014 TIS Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+module CloudConductor
+  module Patches
+    describe AddIAMUser do
+      before do
+        @template = JSON.parse <<-EOS
+          {
+            "Resources": {
+              "Sample": {
+                "Type": "AWS::EC2::RouteTable",
+                "Properties": {
+                  "VpcId": { "Ref": "VPC" }
+                }
+              }
+            }
+          }
+        EOS
+
+        @template = @template.with_indifferent_access
+      end
+
+      it 'extend Patch class' do
+        expect(AddIAMUser.superclass).to eq(Patch)
+      end
+
+      describe '#apply' do
+        before do
+          @patch = AddIAMUser.new
+        end
+
+        it 'add AWS::EC2::IAMUser resource' do
+          expect(@template[:Resources].size).to eq(1)
+          result = @patch.apply @template, {}
+          expect(result[:Resources].size).to eq(2)
+        end
+
+        it 'doesn\'t affect to other resources' do
+          original = @template[:Resources][:Sample].deep_dup
+
+          expect(@template[:Resources][:Sample]).to eq(original)
+          result = @patch.apply @template, {}
+          expect(result[:Resources][:Sample]).to eq(original)
+        end
+
+        it 'doesn\'t affect to source template' do
+          original_template = @template.deep_dup
+
+          expect(original_template).to eq(@template)
+          @patch.apply @template, {}
+          expect(original_template).to eq(@template)
+        end
+      end
+    end
+  end
+end
