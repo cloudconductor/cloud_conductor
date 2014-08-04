d800a9c4f5b280597878fb91dca5015fb10f41b6
Yo Takezawa <takezawa.yo@tis.co.jp>
2014-08-04 14:07:05 +0900

    Add specs for PackerClient#parse

--- lib/cloud_conductor/packer_client.rb
+++ lib/cloud_conductor/packer_client.rb
@@ -30,5 +30,81 @@ module CloudConductor
 
       yield status, stdout, stderr if block_given?
     end
+
+    private
+
+    def parse(stdout)
+      # success
+      # {
+      #   'aws-centos' => {
+      #     status: :success,
+      #     image: 'ami-e18bd5e0'
+      #   },
+      #   'openstack-centos' => {
+      #     status: :success,
+      #     image: '78dd03a1-c9c2-4376-bae9-b84c7b0ca6e1'
+      #   }
+      # }
+
+      # error_aws_image_not_found
+      # {
+      #   'aws-centos' => {
+      #     status: :error,
+      #     message: "Error querying AMI: The image id '[ami-29dc9229]' does not exist (InvalidAMIID.NotFound)"
+      #   }
+      # }
+
+      # error_aws_ssh_faild
+      # {
+      #   'aws-centos' => {
+      #     status: :error,
+      #     message: 'Error waiting for SSH: ssh: handshake failed: ssh: unable to authenticate, attempted methods [none publickey], no supported methods remain'
+      #   }
+      # }
+
+      # error_aws_provisioners_faild
+      # {
+      #   'aws-centos' => {
+      #     status: :error,
+      #     message: 'Script exited with non-zero exit status: 1'
+      #   }
+      # }
+
+      # error_openstack_image_not_found
+      # {
+      #   'openstack-centos' => {
+      #     status: :error,
+      #     message: 'Error launching source server: Expected HTTP response code [202] when accessing URL(http://10.255.197.109:8774/v2/41acd44fb80544d39792509881c00724/servers); got 400 instead with the following body:\n==> openstack-centos: {"badRequest": {"message": "Invalid imageRef provided.", "code": 400}}'
+      #   }
+      # }
+
+      # error_openstack_ssh_faild
+      # {
+      #   'openstack-centos' => {
+      #     status: :error,
+      #     message: 'Error waiting for SSH: ssh: handshake failed: ssh: unable to authenticate, attempted methods [none publickey], no supported methods remain'
+      #   }
+      # }
+
+      # error_openstack_provisioners_faild
+      # {
+      #   'openstack-centos' => {
+      #     status: :error,
+      #     message: 'Script exited with non-zero exit status: 1'
+      #   }
+      # }
+
+      # error_concurrency
+      # {
+      #   'aws-centos' => {
+      #     status: :error,
+      #     message: 'Script exited with non-zero exit status: 1'
+      #   },
+      #   'openstack-centos' => {
+      #     status: :error,
+      #     message: 'Script exited with non-zero exit status: 1'
+      #   }
+      # }
+    end
   end
 end
--- spec/lib/cloud_conductor/packer_client_spec.rb
+++ spec/lib/cloud_conductor/packer_client_spec.rb
@@ -14,19 +14,22 @@
 # limitations under the License.
 module CloudConductor
   describe PackerClient do
+    before do
+      options = {
+        aws_access_key: 'dummy_access_key',
+        aws_secret_key: 'dummy_secret_key',
+        openstack_host: 'dummy_host',
+        openstack_username: 'dummy_user',
+        openstack_password: 'dummy_password',
+        openstack_tenant_id: 'dummy_tenant_id',
+        packer_path: '/opt/packer/packer',
+        packer_json_path: '/tmp/packer.json'
+      }
+      @client = PackerClient.new options
+    end
+
     describe '#build' do
       before do
-        options = {
-          aws_access_key: 'dummy_access_key',
-          aws_secret_key: 'dummy_secret_key',
-          openstack_host: 'dummy_host',
-          openstack_username: 'dummy_user',
-          openstack_password: 'dummy_password',
-          openstack_tenant_id: 'dummy_tenant_id',
-          packer_path: '/opt/packer/packer',
-          packer_json_path: '/tmp/packer.json'
-        }
-        @client = PackerClient.new options
         @clouds = %w(aws openstack)
         @oss = %w(centos ubuntu)
         @role = 'nginx'
@@ -83,6 +86,111 @@ module CloudConductor
 
         @client.should_receive(:systemu).with(pattern)
         @client.build('http://example.com', 'dummy_revision', @clouds, @oss, @role)
+      end
+    end
+
+    describe '#parse' do
+      def load_csv(path)
+        results_path = File.expand_path('../../features/packer_results', File.dirname(__FILE__))
+        csv_path = File.expand_path(path, results_path)
+
+        File.open(csv_path).read
+      end
+
+      it 'return success status and image of all builders when success all builders' do
+        csv = load_csv 'success.csv'
+        result = @client.send(:parse, csv)
+        expect(result.keys).to match_array(%w(aws-centos openstack-centos))
+
+        aws = result['aws-centos']
+        expect(aws[:status]).to eq(:success)
+        expect(aws[:image]).to match(/ami-[0-9a-f]{8}/)
+
+        openstack = result['openstack-centos']
+        expect(openstack[:status]).to eq(:success)
+        expect(openstack[:image]).to match(/[0-9a-f\-]{36}/)
+      end
+
+      it 'return error status and error message about aws builder when source image does not exists while build on aws' do
+        csv = load_csv 'error_aws_image_not_found.csv'
+        result = @client.send(:parse, csv)
+        expect(result.keys).to match_array(%w(aws-centos))
+
+        aws = result['aws-centos']
+        expect(aws[:status]).to eq(:error)
+        expect(aws[:image]).to be_nil
+        expect(aws[:message]).to match(/Error querying AMI: The image id '\[ami-[0-9a-f]{8}\]' does not exist \(InvalidAMIID.NotFound\)/)
+      end
+
+      it 'return error status and error message about aws builder when SSH connecetion failed while build on aws' do
+        csv = load_csv 'error_aws_ssh_faild.csv'
+        result = @client.send(:parse, csv)
+        expect(result.keys).to match_array(%w(aws-centos))
+
+        aws = result['aws-centos']
+        expect(aws[:status]).to eq(:error)
+        expect(aws[:image]).to be_nil
+        expect(aws[:message]).to eq('Error waiting for SSH: ssh: handshake failed: ssh: unable to authenticate, attempted methods [none publickey], no supported methods remain')
+      end
+
+      it 'return error status and error message about aws builder when an error has occurred while provisioning' do
+        csv = load_csv 'error_aws_provisioners_faild.csv'
+        result = @client.send(:parse, csv)
+        expect(result.keys).to match_array(%w(aws-centos))
+
+        aws = result['aws-centos']
+        expect(aws[:status]).to eq(:error)
+        expect(aws[:image]).to be_nil
+        expect(aws[:message]).to match('Script exited with non-zero exit status: \d+')
+      end
+
+      it 'return error status and error message about openstack builder when source image does not exists while build on openstack' do
+        csv = load_csv 'error_openstack_image_not_found.csv'
+        result = @client.send(:parse, csv)
+        expect(result.keys).to match_array(%w(openstack-centos))
+
+        openstack = result['openstack-centos']
+        expect(openstack[:status]).to eq(:error)
+        expect(openstack[:image]).to be_nil
+        expect(openstack[:message]).to match(%r{Error launching source server: Expected HTTP response code \[202\] when accessing URL\(http://[0-9\.]+:8774/v2/[0-9a-f]+/servers\); got 400 instead with the following body:\\n==> openstack-centos: \{"badRequest": \{"message": "Invalid imageRef provided.", "code": 400\}\}})
+      end
+
+      it 'return error status and error message about openstack builder when SSH connecetion failed while build on openstack' do
+        csv = load_csv 'error_openstack_ssh_faild.csv'
+        result = @client.send(:parse, csv)
+        expect(result.keys).to match_array(%w(openstack-centos))
+
+        openstack = result['openstack-centos']
+        expect(openstack[:status]).to eq(:error)
+        expect(openstack[:image]).to be_nil
+        expect(openstack[:message]).to eq('Error waiting for SSH: ssh: handshake failed: ssh: unable to authenticate, attempted methods [none publickey], no supported methods remain')
+      end
+
+      it 'return error status and error message about openstack builder when an error has occurred while provisioning' do
+        csv = load_csv 'error_openstack_provisioners_faild.csv'
+        result = @client.send(:parse, csv)
+        expect(result.keys).to match_array(%w(openstack-centos))
+
+        openstack = result['openstack-centos']
+        expect(openstack[:status]).to eq(:error)
+        expect(openstack[:image]).to be_nil
+        expect(openstack[:message]).to match('Script exited with non-zero exit status: \d+')
+      end
+
+      it 'return error status and error message about all builders when multiple builders failed' do
+        csv = load_csv 'error_concurrency.csv'
+        result = @client.send(:parse, csv)
+        expect(result.keys).to match_array(%w(aws-centos openstack-centos))
+
+        aws = result['aws-centos']
+        expect(aws[:status]).to eq(:error)
+        expect(aws[:image]).to be_nil
+        expect(aws[:message]).to match('Script exited with non-zero exit status: \d+')
+
+        openstack = result['openstack-centos']
+        expect(openstack[:status]).to eq(:error)
+        expect(openstack[:image]).to be_nil
+        expect(openstack[:message]).to match('Script exited with non-zero exit status: \d+')
       end
     end
   end
