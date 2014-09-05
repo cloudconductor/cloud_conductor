b05e8c01da54bba3c620e663beb685fd14ba96d5
Yo Takezawa <takezawa.yo@tis.co.jp>
2014-09-05 11:47:15 +0900

    Clean database while testing more frequently

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -34,8 +34,14 @@ Spork.prefork do
     end
 
     config.before :suite do
-      DatabaseCleaner.strategy = :truncation
-      DatabaseCleaner.clean
+      DatabaseCleaner.strategy = :transaction
+      DatabaseCleaner.clean_with(:truncation)
+    end
+
+    config.around(:each) do |example|
+      DatabaseCleaner.cleaning do
+        example.run
+      end
     end
   end
 end
