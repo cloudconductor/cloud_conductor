0d403958171401786f637978e9da7370048ded1b
Yo Takezawa <takezawa.yo@tis.co.jp>
2014-09-26 09:50:53 +0900

    Support :focus format to restrict specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,6 +27,7 @@ Spork.prefork do
     # Enable focus feature to execute focused test only.
     config.filter_run focus: true
     config.run_all_when_everything_filtered = true
+    config.treat_symbols_as_metadata_keys_with_true_values = true
 
     config.before :all do
       FactoryGirl.factories.clear
