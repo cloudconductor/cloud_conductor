7f30efe893bea9132aadf051d24a4ca71bfc40ca
konda <konda.tomoyuki@tis.co.jp>
2014-10-27 11:58:25 +0900

    Remove unnecessary processing

--- src/controllers/systems_controller.rb
+++ src/controllers/systems_controller.rb
@@ -90,10 +90,6 @@ class SystemsController < Sinatra::Base
       CloudConductor::SystemBuilder.new(system).build
     end
 
-    Thread.new do
-      CloudConductor::SystemBuilder.new(system).build
-    end
-
     status 201
     json system
   end
