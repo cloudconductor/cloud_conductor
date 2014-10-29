92f15518bb4ab8c7920a51e15fe90bbb6ac013f9
Yo Takezawa <takezawa.yo@tis.co.jp>
2014-10-29 09:16:39 +0900

    Fix rubocop violation

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,6 +2,6 @@ worker_processes 4
 pid File.expand_path('unicorn.pid')
 stderr_path File.expand_path('log/unicorn.stderr.log')
 stdout_path File.expand_path('log/unicorn.stdout.log')
-listen ENV['CC_PORT'] || 8080, :tcp_nopush => true
+listen ENV['CC_PORT'] || 8080, tcp_nopush: true
 timeout 1200
 preload_app true
