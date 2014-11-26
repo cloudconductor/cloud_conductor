5fe7861d80c20a368f19a5e00161602cb4e1946d
Yo Takezawa <takezawa.yo@tis.co.jp>
2014-11-26 15:43:25 +0900

    Randomize order of specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -36,6 +36,7 @@ Spork.prefork do
     config.filter_run focus: true
     config.run_all_when_everything_filtered = true
     config.treat_symbols_as_metadata_keys_with_true_values = true
+    config.order = 'random'
 
     config.before :all do
       FactoryGirl.factories.clear
