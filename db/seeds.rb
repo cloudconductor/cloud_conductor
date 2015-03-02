9934b59f0e44b6fadf2e6188721bda7052f8f7e5
Kazuya Takahashi <takahashi.kazuya@tis.co.jp>
2015-03-02 21:11:38 +0900

    Divide test data from db/seed.rb

--- db/seeds.rb
+++
@@ -1,9 +0,0 @@
-if Rails.env == 'development'
-  Account.where(email: 'admin@example.com').first_or_create!(
-    email: 'admin@example.com',
-    name: 'Administrator',
-    password: 'password',
-    password_confirmation: 'password',
-    admin: true
-  )
-end
