eb3b425cb71189151f496075a25ed1d2e36c7589
konda <konda.tomoyuki@tis.co.jp>
2015-04-24 13:32:40 +0900

    Move log_level setting on correct place

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -31,6 +31,8 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
+  config.log_level = :warn
+
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
