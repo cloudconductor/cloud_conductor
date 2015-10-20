9d0b3848e1e62bd7871dd68da0f346c09cda6e85
Masafumi Okamoto <okamoto.masafumi@tis.co.jp>
2015-10-20 10:07:31 +0900

    Modify data migrate logic

--- db/migrate/20151019061413_rename_secret_column_on_clouds.rb
+++ db/migrate/20151019061413_rename_secret_column_on_clouds.rb
@@ -1,5 +1,38 @@
 class RenameSecretColumnOnClouds < ActiveRecord::Migration
-  def change
+  class TransferCloud < ActiveRecord::Base
+    self.table_name = :clouds
+    self.inheritance_column = nil
+  end
+
+  def up
+    cipher = 'aes-256-cbc'
+    secure = CloudConductor::Config.secure.key
+
     rename_column :clouds, :secret, :encrypted_secret
+    TransferCloud.all.each do |cloud|
+      begin
+        crypt = ActiveSupport::MessageEncryptor.new(secure, cipher)
+        value = crypt.encrypt_and_sign(cloud.encrypted_secret)
+        cloud.update_attributes!(encrypted_secret: value)
+      rescue
+        next
+      end
+    end
+  end
+
+  def down
+    cipher = 'aes-256-cbc'
+    secure = CloudConductor::Config.secure.key
+
+    rename_column :clouds, :encrypted_secret, :secret
+    TransferCloud.all.each do |cloud|
+      begin
+        crypt = ActiveSupport::MessageEncryptor.new(secure, cipher)
+        value = crypt.decrypt_and_verify(cloud.secret)
+        cloud.update_attributes!(secret: value)
+      rescue
+        next
+      end
+    end
   end
 end
