d7e4a0d0d1685d046eaeccbf836d1ba6c2d5657b
Yo Takezawa <takezawa.yo@tis.co.jp>
2015-10-23 14:18:05 +0900

    Add spec for /api/v1/blueprints/:id/histories/:id/parameters

--- spec/requests/api/v1/blueprint_histories_spec.rb
+++ spec/requests/api/v1/blueprint_histories_spec.rb
@@ -34,6 +34,37 @@ describe API do
       end
     end
 
+    describe 'GET /blueprints/:blueprint_id/histories/:id' do
+      let(:method) { 'get' }
+      let(:url) { "/api/v1/blueprints/#{blueprint.id}/histories/#{blueprint_history.id}/parameters" }
+      let(:result) { {} }
+
+      context 'not_logged_in' do
+        it_behaves_like('401 Unauthorized')
+      end
+
+      context 'not_exist_id', admin: true do
+        let(:url) { "/api/v1/blueprints/#{blueprint.id}/histories/0/parameters" }
+        it_behaves_like('404 Not Found')
+      end
+
+      context 'normal_account', normal: true do
+        it_behaves_like('403 Forbidden')
+      end
+
+      context 'administrator', admin: true do
+        it_behaves_like('200 OK')
+      end
+
+      context 'project_owner', project_owner: true do
+        it_behaves_like('200 OK')
+      end
+
+      context 'project_operator', project_operator: true do
+        it_behaves_like('200 OK')
+      end
+    end
+
     describe 'DELETE /blueprints/:blueprint_id/histories/:id' do
       let(:method) { 'delete' }
       let(:url) { "/api/v1/blueprints/#{blueprint.id}/histories/#{blueprint_history.id}" }
