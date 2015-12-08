7c8408e123040d677231391368d70ee86faedff8
ko-he- <ko-he-@ex.com>
2015-12-08 10:23:10 +0900

    Clean up audit

--- spec/requests/api/v1/helpers_spec.rb
+++ spec/requests/api/v1/helpers_spec.rb
@@ -12,17 +12,17 @@ module API
         allow(@helpers).to receive(:env).and_return(@dummy_env)
       end
 
-      describe 'trace_api' do
+      describe 'track_api' do
         it 'create log with valid parameters' do
           allow(@helpers).to receive_message_chain(:request, :url).and_return('http://test/api/v1/test')
           allow(@helpers).to receive_message_chain(:request, :ip).and_return('127.0.0.1')
-          allow(@helpers).to receive(:status).and_return(200)
+          allow(@helpers).to receive(:status).and_return('200')
           expect { @helpers.track_api }.to change { Audit.count }.by(1)
           audit = @helpers.track_api
-          expect(audit.account).not_to be_nil
-          expect(audit.ip).not_to be_nil
-          expect(audit.request).not_to be_nil
-          expect(audit.status).not_to be_nil
+          expect(audit.account).to eq(@account.id)
+          expect(audit.ip).to eq('127.0.0.1')
+          expect(audit.request).to eq('http://test/api/v1/test')
+          expect(audit.status).to eq('200')
         end
       end
 
