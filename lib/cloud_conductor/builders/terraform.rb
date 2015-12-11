5ba220d84c5fd245227947022f6bc5f48c551e22
Yo Takezawa <takezawa.yo@tis.co.jp>
2015-12-11 10:49:41 +0900

    Implement Terraform#build_infrastructure to create environment by terraform

--- lib/cloud_conductor/builders/terraform.rb
+++ lib/cloud_conductor/builders/terraform.rb
@@ -11,6 +11,60 @@ module CloudConductor
       private
 
       def build_infrastructure(mappings = {})
+        directory = generate_template(@cloud, @environment, mappings)
+        outputs = execute_terraform(directory)
+        @environment.update_attribute(:ip_address, frontend_addresses(outputs))
+      rescue => e
+        reset
+        raise e
+      end
+
+      def generate_template(cloud, environment, mappings)
+        parent = CloudConductor::Terraform::Parent.new(cloud)
+        environment.blueprint_history.pattern_snapshots.each do |snapshot|
+          parent.modules << CloudConductor::Terraform::Module.new(cloud, snapshot, mappings[snapshot.name])
+        end
+
+        temporary = File.expand_path("../../tmp/terraform/#{SecureRandom.uuid}", File.dirname(__FILE__))
+        FileUtils.mkdir_p temporary unless Dir.exist? temporary
+
+        parent.save("#{temporary}/template.tf")
+        parent.cleanup
+        temporary
+      end
+
+      def execute_terraform(directory)
+        options = {
+          terraform_path: CloudConductor::Config.terraform.path
+        }
+        terraform = Rterraform::Client.new(directory, options)
+
+        # terraform get
+        terraform.get
+
+        # terraform plan
+        variables = {
+          bootstrap_expect: 0
+        }
+        outputs = terraform.plan(variables)
+
+        # terraform apply
+        variables[:bootstrap_expect] = bootstrap_expect(outputs)
+        terraform.apply(variables)
+        terraform.output
+      end
+
+      def bootstrap_expect(outputs)
+        instance_types = %w(aws_instance openstack_compute_instance_v2)
+        outputs['module'].values.inject(0) do |sum, module_output|
+          instance_types.inject(sum) do |sum, type|
+            sum + (module_output[type] || {}).size
+          end
+        end
+      end
+
+      def frontend_addresses(outputs)
+        outputs['module'].values.map { |value| value['frontend_addresses'] }.compact.join(', ')
       end
     end
   end
---
+++ spec/lib/cloud_conductor/builders/terraform_spec.rb
@@ -0,0 +1,162 @@
+module CloudConductor
+  module Builders
+    describe Terraform do
+      include_context 'default_resources'
+
+      let(:cloud_aws) { FactoryGirl.create(:cloud, :aws) }
+
+      before do
+        @builder = Terraform.new cloud_aws, environment
+      end
+
+      describe '#initialize' do
+        it 'keep @cloud' do
+          cloud = @builder.instance_variable_get :@cloud
+          expect(cloud).to eq(cloud_aws)
+        end
+
+        it 'keep @environment' do
+          env = @builder.instance_variable_get :@environment
+          expect(env).to eq(environment)
+        end
+      end
+
+      describe '#build_infrastructure' do
+        before do
+          allow(@builder).to receive(:generate_template)
+          allow(@builder).to receive(:execute_terraform)
+          allow(@builder).to receive(:frontend_addresses)
+          allow(@builder).to receive(:reset)
+        end
+
+        it 'call subroutines except #reset' do
+          expect(@builder).to receive(:generate_template).ordered
+          expect(@builder).to receive(:execute_terraform).ordered
+          expect(@builder).not_to receive(:reset)
+          @builder.send(:build_infrastructure)
+        end
+
+        it 'call #reset when some error has been occurred while processing' do
+          allow(@builder).to receive(:execute_terraform).and_raise
+          expect(@builder).to receive(:reset)
+          expect { @builder.send(:build_infrastructure) }.to raise_error(RuntimeError)
+        end
+      end
+
+      describe '#generate_template' do
+        before do
+          @parent = double(:parent, save: nil, cleanup: nil)
+          @mod = double(:module)
+
+          allow(FileUtils).to receive(:mkdir_p)
+          allow(CloudConductor::Terraform::Parent).to receive(:new).and_return(@parent)
+          allow(CloudConductor::Terraform::Module).to receive(:new).and_return(@mod)
+        end
+
+        it 'will create directory if directory does not exist' do
+          allow(Dir).to receive(:exist?).and_return(false)
+          expect(FileUtils).to receive(:mkdir_p).with(/[0-9a-f\-]{36}/)
+          @builder.send(:generate_template, cloud_aws, environment, {})
+        end
+
+        it 'call Parent#save and Parent#cleanup to generate/cleanup parent template' do
+          expect(@parent).to receive(:save).ordered
+          expect(@parent).to receive(:cleanup).ordered
+          @builder.send(:generate_template, cloud_aws, environment, {})
+        end
+
+        it 'return directory path that contains parent template.tf' do
+          path = @builder.send(:generate_template, cloud_aws, environment, {})
+          expect(path).to match(%r(/tmp/terraform/[0-9a-f\-]{36}$))
+        end
+      end
+
+      describe '#execute_terraform' do
+        before do
+          allow(@builder).to receive(:bootstrap_expect).and_return([])
+
+          @terraform = double(:terraform, get: true, plan: {}, apply: true, output: {})
+          allow(Rterraform::Client).to receive(:new).and_return(@terraform)
+        end
+
+        it 'call subroutines except #reset' do
+          expect(@terraform).to receive(:get).ordered
+          expect(@terraform).to receive(:plan).ordered
+          expect(@terraform).to receive(:apply).ordered
+          expect(@terraform).to receive(:output).ordered
+          @builder.send(:execute_terraform, 'directory')
+        end
+
+        it 'terraform output as hash' do
+          result = @builder.send(:execute_terraform, 'directory')
+          expect(result).to eq({})
+        end
+      end
+
+      describe '#bootstrap_expect' do
+        it 'return size of instances in output that is created by terraform plan for aws' do
+          outputs = {
+            'null_resource' => {},
+            'module' => {
+              'tomcat' => {
+                'aws_instance' => {
+                  'web_server' => {},
+                  'ap_server' => {},
+                  'db_server' => {}
+                }
+              },
+              'zabbix' => {
+                'aws_instance' => {
+                  'web_server' => {}
+                }
+              }
+            }
+          }
+
+          expect(@builder.send(:bootstrap_expect, outputs)).to eq(4)
+        end
+
+        it 'return size of instances in output that is created by terraform plan for openstack' do
+          outputs = {
+            'null_resource' => {},
+            'module' => {
+              'tomcat' => {
+                'openstack_compute_instance_v2' => {
+                  'web_server' => {},
+                  'ap_server' => {},
+                  'db_server' => {}
+                }
+              },
+              'zabbix' => {
+                'openstack_compute_instance_v2' => {
+                  'web_server' => {}
+                }
+              }
+            }
+          }
+
+          expect(@builder.send(:bootstrap_expect, outputs)).to eq(4)
+        end
+      end
+
+      describe '#frontend_addresses' do
+        it 'returns frontend addresses in output hash' do
+          outputs = {
+            'module' => {
+              'tomcat' => {
+                'frontend_addresses' => '203.0.113.1'
+              },
+              'dummy' => {
+              },
+              'zabbix' => {
+                'frontend_addresses' => '203.0.113.2'
+              }
+            }
+          }
+
+          expect(@builder.send(:frontend_addresses, outputs)).to eq('203.0.113.1, 203.0.113.2')
+        end
+      end
+    end
+  end
+end
