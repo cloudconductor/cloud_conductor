fa316ded7a3dc63a0151a0dbfe9d6b85138da4ee
Yo Takezawa <takezawa.yo@tis.co.jp>
2016-02-16 08:31:35 +0900

    Fix rubocop violation

--- lib/cloud_conductor/builders/terraform.rb
+++ lib/cloud_conductor/builders/terraform.rb
@@ -124,7 +124,7 @@ module CloudConductor
         variables
       end
 
-      def cloud_variables(cloud)
+      def cloud_variables(cloud) # rubocop:disable MethodLength
         case cloud.type
         when 'aws'
           {
