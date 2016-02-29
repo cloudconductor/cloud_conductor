31cdefd28901e206ab5146c0b73364cba10ba568
Yo Takezawa <takezawa.yo@tis.co.jp>
2016-02-29 15:53:01 +0900

    Delete backup object on wakame vdc when delete blueprint history

--- spec/lib/cloud_conductor/adapters/wakame_vdc_adapter_spec.rb
+++ spec/lib/cloud_conductor/adapters/wakame_vdc_adapter_spec.rb
@@ -19,10 +19,11 @@ module CloudConductor
 
       describe '#destroy_image' do
         before do
-          @image = double('image', state: 'available')
+          @image = double('image', state: 'available', backup_object_id: 'bo-xxxxxx')
           allow(@image).to receive(:destroy)
 
           allow(Hijiki::DcmgrResource::V1203::Image).to receive_message_chain(:find).and_return(@image)
+          allow(@adapter).to receive(:destroy_backup_object)
         end
 
         it 'execute without exception' do
@@ -40,6 +41,57 @@ module CloudConductor
           expect(@image).not_to receive(:destroy)
 
           @adapter.destroy_image 'wmi-xxxxxx'
+        end
+
+        it 'call #destroy_backup_object with backup_object_id' do
+          expect(@adapter).to receive(:destroy_backup_object).with('bo-xxxxxx')
+          @adapter.destroy_image 'wmi-xxxxxx'
+        end
+      end
+
+      describe '#destroy_backup_object' do
+        before do
+          @stubs = Faraday::Adapter::Test::Stubs.new
+
+          original_method = Faraday.method(:new)
+          allow(Faraday).to receive(:new) do |*args, &block|
+            original_method.call(*args) do |builder|
+              builder.adapter :test, @stubs
+              yield block if block
+            end
+          end
+        end
+
+        it 'request API to delete backup object and return true if success to delete' do
+          @stubs.get('/api/12.03/backup_objects/bo-xxxxxx') { [200, {}, '{}'] }
+          @stubs.delete('/api/12.03/backup_objects/bo-xxxxxx') do
+            [200, {}, '{}']
+          end
+          expect(@adapter.destroy_backup_object('bo-xxxxxx')).to be_truthy
+        end
+
+        it 'return false without delete request when target object already deleted' do
+          @stubs.get('/api/12.03/backup_objects/bo-xxxxxx') { [200, {}, '{ "state": "deleted" }'] }
+          @stubs.delete('/api/12.03/backup_objects/bo-xxxxxx') do
+            fail
+          end
+          expect(@adapter.destroy_backup_object('bo-xxxxxx')).to be_falsey
+        end
+
+        it 'return false without delete request when target object does not exist' do
+          @stubs.get('/api/12.03/backup_objects/bo-xxxxxx') { [404, {}, ''] }
+          @stubs.delete('/api/12.03/backup_objects/bo-xxxxxx') do
+            fail
+          end
+          expect(@adapter.destroy_backup_object('bo-xxxxxx')).to be_falsey
+        end
+
+        it 'raise error when some error occurred while deleting backup object' do
+          @stubs.get('/api/12.03/backup_objects/bo-xxxxxx') { [200, {}, '{}'] }
+          @stubs.delete('/api/12.03/backup_objects/bo-xxxxxx') do
+            [400, {}, '{}']
+          end
+          expect { @adapter.destroy_backup_object('bo-xxxxxx') }.to raise_error RuntimeError
         end
       end
     end
